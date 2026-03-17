9efc5299b8119dac0bdeee0844f5c9e2e9a9dfc9
Fag21 <filebaralemu@gmail.com>
2026-03-17 04:49:12 -0700

    chore(config): configure next.js build and routing options

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "books.google.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+      },
+    ],
+  },
+
   reactCompiler: true,
 };
 
